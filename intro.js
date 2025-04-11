0545dcaae030a70effc7d71a8f45d4d780b3d4e9
IGIHOZO-04 <igihozoangel250@gmail.com>
2025-04-11 16:52:45 +0200

    feat: created our first project with javascript

---
+++ intro.js
@@ -0,0 +1,19 @@
+ //console.log('hello world'); 
+
+ //let width = 4;
+//let height =5;
+//console.log(width*height);
+
+
+let width = 4;
+let height =5;
+console.log(`Width:${width}`);
+console.log(`Height:${height}`);
+
+// area = width*height
+let area =width*height;
+console.log(`Area:${area} cm2`);
+
+//perimeter = 2 * (width+height);
+let perimeter =2 *(width+height);
+console.log(`perimeter:${perimeter} cm`);
