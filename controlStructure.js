6485489de66cac8ca0e51138199d00806d58de59
IGIHOZO-04 <igihozoangel250@gmail.com>
2025-04-11 19:58:40 +0200

    chore:First part of control structure

---
+++ controlStructure.js
@@ -0,0 +1,11 @@
+//if-else control structure
+let age = 32;
+console.log(`age:${age}year's old`)
+if(age >= 18 && age <= 30){
+    console.log("Allowed to vote");
+}else if(age >30){
+    console.log("Not allowed because your over 30 years old");
+}
+else{
+    console.log("Not allowed to vote because you are under 18");
+}
---
+++ operators.js
@@ -0,0 +1,26 @@
+//Arithmetic operators
+let a = 15;
+let b = 5;
+let result = a+b;
+console.log (result);
+
+//Compound Assignment operators
+let x = 5;
+x += 10;
+console.log(x);
+
+//Comparison operators
+let isEqual = (a===b);
+console.log(isEqual);
+
+//Logical operators
+ let logicalResuit =(a > 5 && b < 10);
+ console.log(logicalResuit);
+
+ //String operators
+ let fullName ="Aiden"+" "+"Isaac";
+ console.log(fullName);
+
+ //Terinary operators
+ let max = (a<b)?a :b;
+ console.log(max);
