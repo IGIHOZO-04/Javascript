d4f71c28b1752a2bf94fa2cdc65c736668798438
IGIHOZO-04 <igihozoangel250@gmail.com>
2025-04-16 05:51:41 +0200

    feat:understood control structure

---
+++ switch.js
@@ -0,0 +1,22 @@
+let day ='Wednesday';
+switch(day){
+    case 'Monday':
+    console.log('Today you work online');
+    break;
+    case 'Tuesday':
+        console .log('Today you work Remotely');
+        break;
+        case 'Wednesday':
+            console.log('Today you have a break ');
+            break;
+            case 'Thursday':
+                console.log('Today you work onsite');
+                break;
+                case 'Friday': 
+                    console.log('Today you work remotely');
+                    break;
+                    default :
+                    console.log('Today isn`t a work day');
+                    break;
+
+}
